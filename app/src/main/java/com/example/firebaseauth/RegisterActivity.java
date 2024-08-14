1ae13731e8d4bd9cdfae25b34c6cf41df5aea763
R7i <mabrouk2457@gmail.com>
2024-08-14 17:37:20 +0100

    واجهة جاهزة لتعديلات

--- app/src/main/java/com/example/firebaseauth/RegisterActivity.java
+++ app/src/main/java/com/example/firebaseauth/RegisterActivity.java
@@ -1,14 +1,68 @@
 package com.example.firebaseauth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText inputN,inputE,inputP,inputP2;
+    Button btnR,btnG,btn,btnF;
+    TextView txtS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        txtS = findViewById(R.id.txtSign);
+        inputN = findViewById(R.id.inputNameS);
+        inputE = findViewById(R.id.inputEmailS);
+        inputP = findViewById(R.id.inputPasswordS);
+        inputP2 = findViewById(R.id.inputPassword2S);
+        btnR = findViewById(R.id.btnRegist);
+        txtS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,loginActivity.class);
+                RegisterActivity.this.startActivity(intent);
+                finish();
+            }
+        });
+        btnR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = inputN.getText().toString();
+                String email = inputE.getText().toString();
+                String pass = inputP.getText().toString();
+                String pass2 = inputP2.getText().toString();
+                CheckInformationR(name,email,pass,pass2);
+            }
+        });
+    }
+
+    private void CheckInformationR(String name, String email, String pass, String pass2) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        if (name.isEmpty() || name.length() < 2 || name.length() > 50 || !name.matches("[a-zA-Zأ-ي ]+")) {
+            errorMesseg(inputN,"only alphabetic between 2 and 50");
+        } else if (!email.matches(emailRegex) ) {
+            inputN.setError(null);
+            errorMesseg(inputE,"Invalid email Address.");
+        } else if (!(pass.length()>7)) {
+            errorMesseg(inputP,"At least 8 characters");
+        }else if(!pass.equals(pass2))
+            errorMesseg(inputP2,"bassword does not mach");
+        else Toast.makeText(this, "Call Register Methods", Toast.LENGTH_SHORT).show();
+    }
+
+    private void errorMesseg(EditText input,String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }
--- app/src/main/java/com/example/firebaseauth/loginActivity.java
+++ app/src/main/java/com/example/firebaseauth/loginActivity.java
@@ -2,13 +2,62 @@ package com.example.firebaseauth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class loginActivity extends AppCompatActivity {
+    EditText inputEmail,inputPass;
+    Button btnLog,btnG,btnF;
+    TextView txtLog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        txtLog = findViewById(R.id.txtLog);
+        btnLog = findViewById(R.id.btnLog);
+        btnG = findViewById(R.id.btnG);
+        btnF = findViewById(R.id.btnF);
+        inputEmail = findViewById(R.id.inputEmailLog);
+        inputPass = findViewById(R.id.inputPasswordLog);
+
+
+        btnLog.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                String password = inputPass.getText().toString();
+                CheckInformationLogin(email,password);
+            }
+        });
+        txtLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginActivity.this,RegisterActivity.class);
+                loginActivity.this.startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void CheckInformationLogin(String email,String password) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        if(!email.matches(emailRegex))
+            errorMsg(inputEmail,"Invalid email Address");
+        else if((password.length()<7)) {
+            errorMsg(inputPass,"At least 8 characters");
+        }else Toast.makeText(this, "Call Method login", Toast.LENGTH_SHORT).show();
+    }
+
+    private void errorMsg(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }
