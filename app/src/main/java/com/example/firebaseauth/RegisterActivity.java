74ffac826ba287ee6aa909b78114ee97b2773226
R7i <mabrouk2457@gmail.com>
2024-08-14 21:16:02 +0100

    Athuntication Using EmailPassword 1.0

--- app/src/main/java/com/example/firebaseauth/RegisterActivity.java
+++ app/src/main/java/com/example/firebaseauth/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.firebaseauth;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +12,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class RegisterActivity extends AppCompatActivity {
     EditText inputN,inputE,inputP,inputP2;
     Button btnR,btnG,btn,btnF;
     TextView txtS;
-
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +35,9 @@ public class RegisterActivity extends AppCompatActivity {
         inputP = findViewById(R.id.inputPasswordS);
         inputP2 = findViewById(R.id.inputPassword2S);
         btnR = findViewById(R.id.btnRegist);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
         txtS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,7 +70,39 @@ public class RegisterActivity extends AppCompatActivity {
             errorMesseg(inputP,"At least 8 characters");
         }else if(!pass.equals(pass2))
             errorMesseg(inputP2,"bassword does not mach");
-        else Toast.makeText(this, "Call Register Methods", Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(this, "Call Register Methods", Toast.LENGTH_SHORT).show();
+            RegisterUsingFirebaseAuth(email,pass);
+
+        }
+    }
+
+    private void RegisterUsingFirebaseAuth(String email,String password) {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Registration");
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    sendUserNexActivity();
+                    Toast.makeText(RegisterActivity.this, "Registration successfully", Toast.LENGTH_SHORT).show();
+                }else {
+                    progressDialog.dismiss();
+                    Toast.makeText(RegisterActivity.this, ""+task.getException(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void sendUserNexActivity() {
+        Intent intent = new Intent(RegisterActivity.this,loginActivity.class);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        RegisterActivity.this.startActivity(intent);
+        //startActivity(intent);
     }
 
     private void errorMesseg(EditText input,String s) {
--- app/src/main/java/com/example/firebaseauth/loginActivity.java
+++ app/src/main/java/com/example/firebaseauth/loginActivity.java
@@ -1,19 +1,31 @@
 package com.example.firebaseauth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class loginActivity extends AppCompatActivity {
     EditText inputEmail,inputPass;
     Button btnLog,btnG,btnF;
     TextView txtLog;
+    ProgressDialog progressDlog;
+    FirebaseAuth myAuth;
+    FirebaseUser myUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,8 @@ public class loginActivity extends AppCompatActivity {
         btnF = findViewById(R.id.btnF);
         inputEmail = findViewById(R.id.inputEmailLog);
         inputPass = findViewById(R.id.inputPasswordLog);
+        myAuth = FirebaseAuth.getInstance();
+        myUser = myAuth.getCurrentUser();
 
 
         btnLog.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +48,7 @@ public class loginActivity extends AppCompatActivity {
                 String email = inputEmail.getText().toString();
                 String password = inputPass.getText().toString();
                 CheckInformationLogin(email,password);
+
             }
         });
         txtLog.setOnClickListener(new View.OnClickListener() {
@@ -53,7 +68,38 @@ public class loginActivity extends AppCompatActivity {
             errorMsg(inputEmail,"Invalid email Address");
         else if((password.length()<7)) {
             errorMsg(inputPass,"At least 8 characters");
-        }else Toast.makeText(this, "Call Method login", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Call Method login", Toast.LENGTH_SHORT).show();
+            checkLoginFirebase(email,password);
+        }
+
+    }
+
+    private void checkLoginFirebase(String email,String password) {
+        progressDlog = new ProgressDialog(this);
+        progressDlog.setTitle("Login");
+        progressDlog.setMessage("Please wait..");
+        progressDlog.setCanceledOnTouchOutside(false);
+        progressDlog.show();
+        myAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDlog.dismiss();
+                    sendUserNextActivity();
+                    Toast.makeText(loginActivity.this, "Login successfully", Toast.LENGTH_SHORT).show();
+                }else {
+                    progressDlog.dismiss();
+                    Toast.makeText(loginActivity.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void sendUserNextActivity() {
+        Intent intent = new Intent(loginActivity.this,MainActivity.class);
+        loginActivity.this.startActivity(intent);
+        finish();
     }
 
     private void errorMsg(EditText input, String s) {
